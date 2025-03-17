4412ce8dab603a15718bb07614ff01482c079676
alan <alanl07905@gmail.com>
2025-03-17 12:51:05 -0400

    added color palette to sliders and tabs

--- app/pages/account/page.tsx
+++ app/pages/account/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useUser, useSession } from "@clerk/nextjs";
@@ -15,42 +14,50 @@ import NewExpTab from "@/src/components/Account/NewExpTab";
 /**********************************************************************************/
 // Styling
 const containerStyle: CSSProperties = {
-	display: "flex",
-	flexDirection: "column",
-	alignItems: "center",
-	justifyContent: "center",
-	minHeight: "80vh",
-	padding: "20px",
-	width: "100%"
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "80vh",
+  padding: "20px",
+  width: "100%",
 };
 /**********************************************************************************/
 
 // TabPanel component
 interface TabPanelProps {
-	children?: React.ReactNode;
-	index: number;
-	value: number;
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
 function TabPanel(props: TabPanelProps) {
-	const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-	return (
-		<div
-			role="tabpanel"
-			hidden={value !== index}
-			id={`simple-tabpanel-${index}`}
-			aria-labelledby={`simple-tab-${index}`}
-			style={{ width: "100%", display: "flex", justifyContent: "center" }}
-			{...other}
-		>
-			{value === index && (
-				<Box sx={{ pt: 3, width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-					{children}
-				</Box>
-			)}
-		</div>
-	);
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      style={{ width: "100%", display: "flex", justifyContent: "center" }}
+      {...other}
+    >
+      {value === index && (
+        <Box
+          sx={{
+            pt: 3,
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {children}
+        </Box>
+      )}
+    </div>
+  );
 }
 
 export default function AccountPage() {
@@ -65,27 +72,65 @@ export default function AccountPage() {
 
   if (!user) {
     return (
-			<Box sx={containerStyle}>
-				<Typography variant="h6">Please sign in to view your account.</Typography>
-			</Box>
-		);
-  } 
+      <Box sx={containerStyle}>
+        <Typography variant="h6">Please sign in to view your account.</Typography>
+      </Box>
+    );
+  }
 
   return (
     <Box sx={containerStyle}>
-      <Box sx={{ width: '100%', maxWidth: '1000px', mb: 4, borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
-				<Tabs 
-					value={tabValue} 
-					onChange={handleTabChange} 
-					centered
-					textColor="primary"
-					indicatorColor="primary"
-				>
-					<Tab label="INFO" />
-					<Tab label="STATUS" />
-					<Tab label="NEW EXP" />
-				</Tabs>
-			</Box>
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "1000px",
+          mb: 4,
+          borderBottom: 1,
+          borderColor: "divider",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Tabs
+          value={tabValue}
+          onChange={handleTabChange}
+          centered
+          textColor="inherit"
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "#CC0000", 
+            },
+          }}
+        >
+          <Tab
+            label="INFO"
+            sx={{
+              color: "#CC0000",
+              "&.Mui-selected": {
+                color: "#CC0000", 
+              },
+            }}
+          />
+          <Tab
+            label="STATUS"
+            sx={{
+              color: "#CC0000",
+              "&.Mui-selected": {
+                color: "#CC0000",
+              },
+            }}
+          />
+          <Tab
+            label="NEW EXP"
+            sx={{
+              color: "#CC0000",
+              "&.Mui-selected": {
+                color: "#CC0000",
+              },
+            }}
+          />
+        </Tabs>
+      </Box>
 
       {/* Tab Panels */}
       <TabPanel value={tabValue} index={0}>
